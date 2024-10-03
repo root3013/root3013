8bab55fa916598400c6b7fcbc3bac0d3e1fff26e
root3013 <89790567+root3013@users.noreply.github.com>
2024-10-04 01:30:48 +0800

    Add files via upload
    可以自己输入多久关机和取消关机

---
+++ 计划关机窗口版本.py
@@ -0,0 +1,37 @@
+import tkinter as tk
+import os
+window=tk.Tk()#窗口容器（主要窗口）
+window.title('计划关机  作者:wzl')#窗口标题
+window.geometry('800x600')#容器大小
+window.configure(background='grey')
+label_top=tk.Label(window,text='键入关机时间单位s',bg='green',width=5000)
+entry=tk.Entry(window)
+text=tk.Text(window,height=2)#heigh决定文本框几行字
+var=tk.StringVar()
+var.set('确定')
+def shutdown():   
+    long="shutdown -s -t "
+    time=entry.get()
+    if time.isnumeric():
+        time=str(time)
+        long=f"{long}{time}"
+        text.delete(1.0, tk.END)
+        text.insert('insert',f" 关机时间为_{time}s")
+        os.system(long)
+        var.set('设置成功')
+    else:
+        var.set('什么玩意')
+        text.insert('insert','非法输入,请输入数字')
+def cancel():
+    quxiao='shutdown -a'
+    text.delete(1.0, tk.END)
+    text.insert('insert',"取消成功")
+    os.system(quxiao)
+button_1=tk.Button(window,textvariable=var,command=shutdown)
+button_2=tk.Button(window,text='取消计划关机',command=cancel)
+label_top.pack()
+entry.pack()
+button_1.pack()
+button_2.pack()
+text.pack()
+window.mainloop()
